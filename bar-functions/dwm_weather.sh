1e8c246ad8abf509ed1fa8310d9b121a544b7046
Deryugin <deryugin.denis@gmail.com>
2020-05-04 21:22:43 +0300

    dwm_weather: Update weather once every 5 minutes

--- bar-functions/dwm_weather.sh
+++ bar-functions/dwm_weather.sh
@@ -7,16 +7,31 @@
 # Dependencies: curl
 
 # Change the value of LOCATION to match your city
-dwm_weather() {
-    LOCATION=city
 
-    printf "%s" "$SEP1"
+WEATHER_FILE=/tmp/dwm_weather
+
+dwm_weather_update() {
+    LOCATION=spb
+
+    printf "%s" "$SEP1" > $WEATHER_FILE
     if [ "$IDENTIFIER" = "unicode" ]; then
-        printf "%s" "$(curl -s wttr.in/$LOCATION?format=1)"
+        printf "%s" "$(curl -s wttr.in/$LOCATION?format=1)" >> $WEATHER_FILE
     else
-        printf "WEA %s" "$(curl -s wttr.in/$LOCATION?format=1 | grep -o "[0-9].*")"
+        printf "WEA %s" "$(curl -s wttr.in/$LOCATION?format=1 | grep -o "[0-9].*")" >> $WEATHER_FILE
     fi
-    printf "%s\n" "$SEP2"
+    printf "%s\n" "$SEP2" >> $WEATHER_FILE
+}
+
+dwm_weather() {
+	# Force update if file doesn't exist
+	[ ! -f "$WEATHER_FILE" ] && dwm_weather_update
+
+	# Update if file is older than 5 minutes
+	if test "`find $WEATHER_FILE -mmin +5`"; then
+		dwm_weather_update
+	fi
+	
+	cat $WEATHER_FILE
 }
 
 dwm_weather
